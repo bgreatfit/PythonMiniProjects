94237b668d0898898b3e2cee4f1240d7a2029696
micheal <micheal@pearlstack.com>
2018-10-11 08:24:05 +0100

    fixed installing web driver for chrome

--- AutomateWebform/file.py
+++ AutomateWebform/file.py
@@ -7,7 +7,6 @@ dir_list = os.listdir(submission_dir)
 file_tup = {}
 for directory in dir_list:
     file_list = list(os.listdir(os.path.join(submission_dir, directory)))
-
     if len(file_list) != 0:
         print(file_list[0])
         file_tup[directory] = file_list[0]
